d7e4cf5e111ee187e0d566c1d483b0d79464743b
Oware-Ebenzer <nanaprestige71@gmail.com>
2025-10-29 17:27:27 +0000

    Config main.js

--- ticket-app/src/main.js
+++ ticket-app/src/main.js
@@ -1,9 +1,27 @@
+import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
+
+
+import Vue3Toastify, { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(router)
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  theme: 'dark',
+  position: 'top-right',
+  transition: 'bounce',
+})
+
 
 app.mount('#app')
+
+
+export { toast }
